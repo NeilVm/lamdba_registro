1400928c530569b0a02a7ba5362ea4255ea9c7e8
Neil Vela Marchena <neil_vela@usmp.pe>
2023-09-26 18:46:16 -0500

    correcciones

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -28,27 +28,27 @@ namespace registro.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Verificar si el correo ya está en uso (puedes agregar validación adicional aquí)
+                
                 var usuarioExistente = _context.Registros.FirstOrDefault(r => r.Correo == registro.Correo);
                 if (usuarioExistente != null)
                 {
                     return BadRequest("El correo ya está registrado.");
                 }
 
-                // Generar un hash de contraseña antes de almacenarla
-                string contrasenaPlana = registro.ContrasenaHash; // Aquí obtienes la contraseña proporcionada por el usuario
+                
+                string contrasenaPlana = registro.ContrasenaHash; 
                 string contrasenaHash = BCrypt.Net.BCrypt.HashPassword(contrasenaPlana);
 
-                // Almacenar el hash de contraseña en el modelo de usuario
+                
                 registro.ContrasenaHash = contrasenaHash;
 
-                _context.Registros.Add(registro); // Agrega el registro al contexto
-                _context.SaveChanges(); // Guarda los cambios en la base de datos
+                _context.Registros.Add(registro); 
+                _context.SaveChanges(); 
 
                 return Ok("Registro guardado exitosamente.");
             }
 
-            return BadRequest("Los datos del registro no son válidos."); // Si la validación falla
+            return BadRequest("Los datos del registro no son válidos."); 
         }
 
 
